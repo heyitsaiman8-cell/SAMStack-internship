a1aef1f6c11cee195775d2ccb73a92bc78fc7eb8
heyitsaiman8-cell <heyitsaiman8@gmail.com>
2026-08-20 08:07:03 -0700

    Add Python practice set

---
+++ python basics.py
@@ -0,0 +1,46 @@
+# practice set
+a={1,2,3,6}
+b={5,7,9,8}
+print(a.union(b))
+
+c=str(input("Enter name="))
+print("hello ",c)
+
+letter="hello (name) is a student of uet."
+print(letter.replace("(name)","ali"))
+print(letter)
+
+fruits=[]
+f1=input("enter number 1:")
+fruits.append(f1)
+
+f2=input("enter number 1:")
+fruits.append(f2)
+
+f3=input("enter number 1:")
+fruits.append(f3)
+
+f4=input("enter number 1:")
+fruits.append(f4)
+
+f5=input("enter number 1:")
+fruits.append(f5)
+
+f6=input("enter number 1:")
+fruits.append(f6)
+
+f7=input("enter number 1:")
+fruits.append(f7)
+
+print(fruits)
+# list and tuples
+a=[1,2,9,"lahore"]
+b=(1,5,6,"ali")
+print(a.append("karachi"))
+print(a)
+print(a.index(2))
+
+print(type(b))
+
+a=[2,4,5,6,1]
+print(sum(a))
