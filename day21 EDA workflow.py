4854a2cc441d389b6fbd673133c3aa4b93d6d809
heyitsaiman8-cell <heyitsaiman8@gmail.com>
2026-08-29 09:11:21 -0700

    EDA flow

--- day21 EDA workflow.py
+++ day21 EDA workflow.py
@@ -3,16 +3,11 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from scipy.stats import zscore
 
-# ==========================================
-# DAY 21 - EXPLORATORY DATA ANALYSIS
-# ==========================================
-
-# 1. Load Dataset
+print(" ==========================================")
+print("DAY 21 - EXPLORATORY DATA ANALYSIS")
+print(" ==========================================")
 df = pd.read_csv("student_performance.csv")
-
 print("========== EDA REPORT ==========")
-
-# 2. Basic Structure
 print("\n--- Dataset Shape ---")
 print(df.shape)
 
@@ -22,23 +17,19 @@ print(df.head())
 print("\n--- Column Names ---")
 print(df.columns.tolist())
 
-# 3. Dataset Information
 print("\n--- Dataset Information ---")
 df.info()
 
-# 4. Missing Values
 print("\n--- Missing Values ---")
 print(df.isnull().sum())
 
-# 5. Statistical Summary
 print("\n--- Statistical Summary ---")
 print(df.describe())
 
-# 6. Skewness
 print("\n--- Skewness ---")
 print(df.skew(numeric_only=True))
 
-# 7. Distribution
+print("Distribution")
 plt.figure(figsize=(8, 5))
 
 df["Exam_Score"].hist()
@@ -48,7 +39,7 @@ plt.xlabel("Exam Score")
 plt.ylabel("Frequency")
 plt.show()
 
-# 8. Boxplot
+print(" Boxplot")
 plt.figure(figsize=(8, 5))
 
 sns.boxplot(x=df["Exam_Score"])
@@ -57,25 +48,25 @@ plt.title("Exam Score Outliers")
 plt.xlabel("Exam Score")
 plt.show()
 
-# 9. Z-Score
+print(" Z-Score")
 df["Exam_ZScore"] = zscore(df["Exam_Score"])
 
 print("\n--- Exam Scores with Z-Scores ---")
 print(df[["Exam_Score", "Exam_ZScore"]])
 
-# Identify Outliers
+print(" Identify Outliers")
 outliers = df[abs(df["Exam_ZScore"]) > 3]
 
 print("\n--- Potential Outliers ---")
 print(outliers)
 
-# 10. Correlation Matrix
+print(" Correlation Matrix")
 correlation = df.corr(numeric_only=True)
 
 print("\n--- Correlation Matrix ---")
 print(correlation)
 
-# 11. Correlation Heatmap
+print(" Correlation Heatmap")
 plt.figure(figsize=(9, 6))
 
 sns.heatmap(
